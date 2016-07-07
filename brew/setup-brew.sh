da5603addcbcd2717bc5e27a5dabeff4cd5012bb
Nicolas Langley <nicolasqlangley@gmail.com>
2016-07-07 15:36:35 -0700

    Add dotbot and setup scripts

---
+++ brew/setup-brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install Homebrew & brew-cask
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install brew-cask
+brew tap homebrew/versions
+brew tap caskroom/versions
+
+# Install brew & brew-cask packages
+. "$DOTFILES_DIR/brew/brew.sh" "core"
+. "$DOTFILES_DIR/brew/brew-cask.sh" "core"
---
+++ zsh/setup-zsh.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
