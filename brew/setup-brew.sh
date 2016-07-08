a555f960ba6eda38eb8878b37f0f3e2c7e2b1c2e
Nicolas Langley <nicolasqlangley@gmail.com>
2016-07-08 10:12:26 -0700

    Update tmux config

--- brew/setup-brew.sh
+++ brew/setup-brew.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Install Homebrew & brew-cask
-
+# Install brew & brew cask
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 brew install brew-cask
 brew tap homebrew/versions
 brew tap caskroom/versions
 
-# Install brew & brew-cask packages
+# Install brew & brew cask packages
 . "$DOTFILES_DIR/brew/brew.sh" "core"
+. "$DOTFILES_DIR/brew/brew-cask.sh" "core"
