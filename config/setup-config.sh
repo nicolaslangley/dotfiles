1e35ead33b794a144b67fa218ee4a4986aa38729
Nicolas Langley <nicolasqlangley@gmail.com>
2016-07-08 10:10:42 -0700

    Add osx config and cleanup setup files

---
+++ config/setup-config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Setup OSX settings
+. "$DOTFILES_DIR/config/osx-settings.sh"
---
+++ themes/setup-themes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Install powerline fonts for themes
+. "$DOTFILES_DIR/themes/powerline-fonts/install.sh"
