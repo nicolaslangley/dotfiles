92a22fc780ff87047a88e7e812563b80168fea0e
Nicolas Langley <nicolasqlangley@gmail.com>
2016-07-12 15:10:26 -0700

    Add RTC-output shortcut

--- zsh/rtc-env.sh
+++ zsh/rtc-env.sh
@@ -2,4 +2,5 @@
 alias RTC="cd ~/Development/Quartz"
 alias RTC-code="cd ~/Development/Quartz/runtimecore"
 alias RTC-scripts="cd ~/Development/Quartz/runtimecore_scripts/scripts"
+alias RTC-output="cd ~/Development/Quartz/output/macos_x64_debug/bin"
 alias editrtc="pushd ~/Development/Quartz/runtimecore && nvim && popd"
