58c62fda86413a941430964eb3809cd977f6a8f6
Nicolas Langley <nicolasqlangley@gmail.com>
2016-07-14 14:36:03 -0700

    Add lldbinit and setup files

---
+++ lldb/setup-lldb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DOTFILES=~/.dotfiles
+
+# Copy LLDB startup script for OSX system python
+sudo cp $DOTFILES/lldb/lldbinit-start/lldbinit.py /Library/Python/2.7/site-packages
+
+
---
+++ python/pythonrc.py
@@ -0,0 +1,3 @@
+import sys
+sys.path = sys.path + ["/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Resources/Python"]
+
