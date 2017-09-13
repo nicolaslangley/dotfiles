0d346c54bc4e91fb57fa3477a714517d4ff8902b
Nicolas Langley <nicolasqlangley@gmail.com>
2017-09-13 15:38:53 -0700

    Add RTC environment for Android builds

--- zsh/rtc-env.sh
+++ zsh/rtc-env.sh
@@ -4,6 +4,22 @@ alias RTC-code="cd ~/Development/Quartz/runtimecore"
 alias RTC-scripts="cd ~/Development/Quartz/runtimecore_scripts/scripts"
 alias RTC-output="cd ~/Development/Quartz/output/macos_x64_debug/bin"
 alias editrtc="pushd ~/Development/Quartz/runtimecore && nvim && popd"
-alias qmake-ios="~/Development/Qt5.7.0/5.7/ios/bin/qmake"
-alias qmake-qml-app="~/Development/Qt5.7.0/5.7/ios/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml/runtimecore_test_qml.pro -r -spec macx-ios-clang CONFIG+=iphoneos CONFIG+=qml_debug"
 alias git-clean-rtc="~/.dotfiles/scripts/git_clean_branches_rtc.sh"
+alias qmake-ios="~/Development/Qt/5.6/ios/bin/qmake"
+alias qmake-qml-ios-3d-app="~/Development/Qt/5.6/ios/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r CONFIG+=iphoneos CONFIG+=qml_debug"
+alias qmake-qml-android-3d-app="~/Development/Qt/5.6/android_armv7/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r -spec android-g++ CONFIG+=debug CONFIG+=qml_debug"
+alias androiddeployqt="~/Development/Qt/5.6/android_armv7/bin/androiddeployqt"
+alias android-qml-deploy="make install INSTALL_ROOT=\"Debug-android\" && ~/Development/Qt/5.6/android_armv7/bin/androiddeployqt --output ./Debug-android/ --deployment bundled --android-platform android-23 --gradle"
+alias adb="~/Development/Android/sdk/platform-tools/adb" 
+
+
+# Exports for Android toolchains
+export ANDROID_SDK_ROOT=/Users/nico8506/Development/Android/sdk
+export ANDROID_NDK_ROOT=/Users/nico8506/Development/Android/android-ndk-r12b
+export ANDROID_TOOLCHAIN_ROOT=/Users/nico8506/Development/Android/android-ndk-r10e/toolchains
+export ANDROID_ARMV7=${ANDROID_TOOLCHAIN_ROOT}/android-14_arm-linux-androideabi-4.9/bin
+export ANDROID_X86=${ANDROID_TOOLCHAIN_ROOT}/android-14_x86-4.9/bin
+export ANDROIDGLES3_ARMV7=${ANDROID_TOOLCHAIN_ROOT}/android-21_arm-linux-androideabi-4.9/bin
+export ANDROIDGLES3_ARMV8=${ANDROID_TOOLCHAIN_ROOT}/android-21_aarch64-linux-android-4.9/bin
+export ANDROIDGLES3_X86=${ANDROID_TOOLCHAIN_ROOT}/android-21_x86-4.9/bin
+export ANDROIDGLES3_X64=${ANDROID_TOOLCHAIN_ROOT}/android-21_x86_64-4.9/bin
