1f6bab8cb97d30e7b289faa8be4d932bf41daff7
Nicolas Langley <nicolasqlangley@gmail.com>
2017-09-13 16:00:34 -0700

    Add additional alias to RTC-env

--- zsh/rtc-env.sh
+++ zsh/rtc-env.sh
@@ -1,19 +1,18 @@
-# Aliases
+# RTC specific aliases
+
+# Directory aliases
 alias RTC="cd ~/Development/Quartz"
 alias RTC-code="cd ~/Development/Quartz/runtimecore"
+alias RTC-mr3d-dir="cd ~/Development/Quartz/runtimecore/map_renderer_3d"
+alias RTC-mr3d-pipeline="cd ~/Development/Quartz/runtimecore/map_renderer_3d/src/graphics_pipeline3d"
+alias RTC-mr3d-low-level="cd ~/Development/Quartz/runtimecore/map_renderer_3d/src/graphics_pipeline3d/low_level_renderer"
 alias RTC-scripts="cd ~/Development/Quartz/runtimecore_scripts/scripts"
 alias RTC-output="cd ~/Development/Quartz/output/macos_x64_debug/bin"
 alias editrtc="pushd ~/Development/Quartz/runtimecore && nvim && popd"
-alias git-clean-rtc="~/.dotfiles/scripts/git_clean_branches_rtc.sh"
-alias qmake-ios="~/Development/Qt/5.6/ios/bin/qmake"
-alias qmake-qml-ios-3d-app="~/Development/Qt/5.6/ios/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r CONFIG+=iphoneos CONFIG+=qml_debug"
-alias qmake-qml-android-3d-app="~/Development/Qt/5.6/android_armv7/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r -spec android-g++ CONFIG+=debug CONFIG+=qml_debug"
-alias androiddeployqt="~/Development/Qt/5.6/android_armv7/bin/androiddeployqt"
-alias android-qml-deploy="make install INSTALL_ROOT=\"Debug-android\" && ~/Development/Qt/5.6/android_armv7/bin/androiddeployqt --output ./Debug-android/ --deployment bundled --android-platform android-23 --gradle"
+alias git_clean_rtc="~/.dotfiles/scripts/git_clean_branches_rtc.sh"
+
+# Android NDK and Toolchains
 alias adb="~/Development/Android/sdk/platform-tools/adb" 
-
-
-# Exports for Android toolchains
 export ANDROID_SDK_ROOT=/Users/nico8506/Development/Android/sdk
 export ANDROID_NDK_ROOT=/Users/nico8506/Development/Android/android-ndk-r12b
 export ANDROID_TOOLCHAIN_ROOT=/Users/nico8506/Development/Android/android-ndk-r10e/toolchains
@@ -23,3 +22,25 @@ export ANDROIDGLES3_ARMV7=${ANDROID_TOOLCHAIN_ROOT}/android-21_arm-linux-android
 export ANDROIDGLES3_ARMV8=${ANDROID_TOOLCHAIN_ROOT}/android-21_aarch64-linux-android-4.9/bin
 export ANDROIDGLES3_X86=${ANDROID_TOOLCHAIN_ROOT}/android-21_x86-4.9/bin
 export ANDROIDGLES3_X64=${ANDROID_TOOLCHAIN_ROOT}/android-21_x86_64-4.9/bin
+export PATH=${PATH}:${ANDROIDGLES3_ARMV7}:${ANDROIDGLES3_ARMV8}:${ANDROIDGLES3_X86}:${ANDROIDGLES3_X64}
+
+alias qmake-ios="~/Development/Qt/5.6/ios/bin/qmake"
+alias qmake-qml-ios-3d-app="~/Development/Qt/5.6/ios/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r CONFIG+=iphoneos CONFIG+=qml_debug"
+alias qmake-qml-android-3d-app="~/Development/Qt/5.6/android_armv7/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r -spec android-g++ CONFIG+=debug CONFIG+=qml_debug"
+alias androiddeployqt="~/Development/Qt/5.6/android_armv7/bin/androiddeployqt"
+alias android-qml-deploy="make install INSTALL_ROOT=\"Debug-android\" && ~/Development/Qt/5.6/android_armv7/bin/androiddeployqt --output ./Debug-android/ --deployment bundled --android-platform android-23 --gradle"
+
+
+# Qt
+alias qmake-ios="~/Development/Qt/5.6/ios/bin/qmake"
+alias qmake-android="~/Development/Qt/5.6/android_armv7/bin/qmake"
+alias qmake-mac-qml-app="qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml/runtimecore_test_qml.pro -r CONFIG+=debug"
+alias qmake-qml-ios-3d-app="~/Development/Qt/5.6/ios/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r CONFIG+=iphoneos CONFIG+=qml_debug"
+alias qmake-qml-android-3d-app="~/Development/Qt/5.6/android_armv7/bin/qmake ~/Development/Quartz/runtimecore/test/runtimecore_test_qml_3d/runtimecore_test_qml_3d.pro -r -spec android-g++ CONFIG+=debug CONFIG+=qml_debug"
+alias androiddeployqt="~/Development/Qt/5.6/android_armv7/bin/androiddeployqt"
+alias android-qml-deploy="make install INSTALL_ROOT=\"Debug-android\" && ~/Development/Qt/5.6/android_armv7/bin/androiddeployqt --output ./Debug-android/ --deployment bundled --android-platform android-23 --gradle"
+
+# RTC-script aliases
+alias rtc_repos_status="cd ~/Development/Quartz/runtimecore_scripts/scripts && ./get_repos_status.sh -g repos_runtimecore && popd"
+alias rtc_create_build="cd ~/Development/Quartz/runtimecore_scripts/scripts && ./create_build.sh && popd"
+alias rtc_build_runtimecoretest="cd ~/Development/Quartz/runtimecore_scripts/scripts && ./build.sh -s runtimecore_test && popd"
