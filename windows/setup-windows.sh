d7f6e11f437111b6e22c8b729b5928d9ba0dbd9c
Nicolas Langley <nlangley@esri.com>
2018-06-15 11:25:17 -0700

    Update windows dotfiles folder
    Signed-off-by: Nicolas Langley <nlangley@esri.com>

---
+++ windows/setup-windows.sh
@@ -0,0 +1,8 @@
+cp ~/Development/dotfiles/bash_profile_windows ~/.bash_profile
+cp ~/Development/dotfiles/dir_colors_solarized ~/.dir_colors
+cp ~/Development/dotfiles/gitconfig_windows ~/.gitconfig
+
+mkdir -p ~/.bash/themes/agnoster-bash
+cp ~/Development/dotfiles/windows/themes/agnoster-bash/* ~/.bash/themes/agnoster-bash/
+
+ln -s ~/Development/dotfiles ~/.dotfiles
