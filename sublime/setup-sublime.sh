58d21fc2275003785c11b56d172ad2426e0f9b87
Nicolas Langley <nicolasqlangley@gmail.com>
2019-08-12 16:53:19 -0700

    Add sublime text and sublime merge settings to dotfiles
    Signed-off-by: Nicolas Langley <nicolasqlangley@gmail.com>

---
+++ sublime/setup-sublime.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  # Copy settings for Sublime Text
+  cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+  rm -r User
+  ln -s ~/dotfiles/sublime/text/User
+  # Copy settings for Sublime Merge
+  cd ~/Library/Application\ Support/Sublime\ Merge/Packages/
+  rm -r User
+  ln -s ~/dotfiles/sublime/merge/User
+else
+  # Assume Linux platforms if not MacOS
+  # Copy settings for Sublime Text
+  cd ~/.config/sublime-text-3/Packages/
+  rm -r User
+  ln -s ~/dotfiles/sublime/text/User
+  # Copy settings for Sublime Merge
+  cd ~/.config/sublime_merge/Packages/
+  rm -r User
+  ln -s ~/dotfiles/sublime/merge/User
+fi
