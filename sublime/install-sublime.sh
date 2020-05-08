36be5b215adad57761440a2ccf623d03a149e82c
Nicolas Langley <nicolasqlangley@gmail.com>
2020-05-08 10:05:32 -0700

    Remove some sublime packages and add install script
    Signed-off-by: Nicolas Langley <nicolasqlangley@gmail.com>

---
+++ sublime/install-sublime.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  # Download Dev track of Sublime Text & Sublime Merge
+  curl -o ~/Downloads/sublime_text_build_4074_mac.zip https://download.sublimetext.com/sublime_text_build_4074_mac.zip 
+  curl -o ~/Downloads/sublime_merge_build_2009_mac.zip https://download.sublimetext.com/sublime_merge_build_2009_mac.zip
+fi
+
+
+
+
