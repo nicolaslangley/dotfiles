d745ea57ca1aafa4506e7dbdbfb11e7c5e832248
Nicolas Langley <nicolasqlangley@gmail.com>
2021-06-08 09:36:51 -0700

    Update sourcetrail custom theme
    Signed-off-by: Nicolas Langley <nicolasqlangley@gmail.com>

--- themes/setup-themes.sh
+++ themes/setup-themes.sh
@@ -4,3 +4,10 @@
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 cp -f $DOTFILES_DIR/themes/xcode/Ayu\ Mirage.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
+# Copy custom sourcetrail theme if installed
+SOURCETRAIL_COLORSCHEME_DIR=/Applications/Sourcetrail.app/Contents/Resources/data/color_schemes/
+if [ -d "$SOURCETRAIL_COLORSCHEME_DIR" ]; then
+  cp -f $DOTFILES_DIR/themes/sourcetrail/custom.xml $SOURCETRAIL_COLORSCHEME_DIR
+fi
+
+
