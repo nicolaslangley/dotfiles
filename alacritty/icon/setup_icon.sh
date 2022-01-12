3f7194c07d9a330baefab1847beb69387877cf52
Nicolas Langley <nicolaslangley@hey.com>
2022-01-11 17:03:16 -0800

    Update setup icon script
    Signed-off-by: Nicolas Langley <nicolaslangley@hey.com>

--- alacritty/icon/setup_icon.sh
+++ alacritty/icon/setup_icon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Update alacritty icon
-iconutil -c icns ~/Downloads/alacritty.iconset -o /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+iconutil -c icns $DOTFILES_DIR/alacritty/icon/alacritty.iconset -o /Applications/Alacritty.app/Contents/Resources/alacritty.icns
 # Clear MacOS icon cache
 sudo rm -rfv /Library/Caches/com.apple.iconservices.store; sudo find /private/var/folders/ \( -name com.apple.dock.iconcache -or -name com.apple.iconservices \) -exec rm -rfv {} \; ; sleep 3;sudo touch /Applications/* ; killall Dock; killall Finder
